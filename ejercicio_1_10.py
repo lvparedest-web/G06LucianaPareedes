84626902b6fcf517fff8fa2c0ad35c056537ac00
lvparedest <lvparedest@gmail.com>
2026-08-16 23:34:21 -0500

    Completé ejercicio 1.10

---
+++ ejercicio_1_10.py
@@ -0,0 +1,10 @@
+a=[1,2,3,4,5]
+print(a[2:4])
+#a[2:4]=[6,7,8,9]    error de sintaxis
+print(a)
+print(a[1:-3])
+#a[1:-3]=[3:4]        error de sintaxis
+print(a)
+print(a[1:-1])
+#a[1:-1]=[]       error de sintaxis
+print(a)
